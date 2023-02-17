a5fe60ff9518ac08019591c0b8311b16a553d43e
jashan <jashannoor2001@gmail.com>
2023-02-17 15:58:39 -0500

    Added Files

--- app.js
+++ app.js
@@ -6,39 +6,47 @@ const logger = require('morgan');
 
 const indexRouter = require('./controllers/index');
 const usersRouter = require('./controllers/users');
+// reference our new custom controllers
+const employers = require('./controllers/employers');
+const cities = require('./controllers/cities');
 
 const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+
 app.set('view engine', 'hbs');
+
+
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-//for using dependencies
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
-
 // use dotenv to read .env file with config vars
-if(process.env.Node_ENV !='production'){
-
+if (process.env.NODE_ENV != 'production') {
   require('dotenv').config()
-
 }
 
-const mongoose=require('mongoose');
+// mongodb connection using mongoose
+const mongoose = require('mongoose');
 
 mongoose.connect(process.env.CONNECTION_STRING)
-.then((res)=>{
-  console.log('Connected To Moongoose')
-}).catch(()=>{
-  console.log('Connection To Moongoose Failed')
+.then((res) => {
+  console.log('Connected to MongoDB');
 })
+.catch(() => {
+  console.log('Connection to MongoDB Failed');
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// map all requests at /employers to our own employers.js controller
+app.use('/employers', employers);
+app.use('/cities', cities);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- controllers/employers.js
+++ controllers/employers.js
@@ -1,31 +1,65 @@
-const express=require('express');
-const router=express.Router()
-//add fsfile
-const fs=require('fs');
+const express = require('express');
+const router = express.Router();
 
+// add fs to read the data file
+const fs = require('fs');
 
+// use Employer model for CRUD w/mongoose
+const Employer = require('../models/employer');
 
-//Get employers index
+/* GET employers index (the module home page) */
+router.get('/', (req, res) => {
+    /*const employers = [
+        {
+            "name": "Provix"
+        },
+        {
+            "name": "Element6"
+        },
+        {
+            "name": "Netgain"
+        },
+        {
+            "name": "44 North"
+        }
+    ];
 
-router.get('/',(req,res)=>{
+    res.render('employers/index', {
+        title: 'Employer List',
+        employers: employers
+    });*/
 
-   
-    
     // get data from json file
-    fs.readFile("./data/employers.json",'utf8',(error,employers)=>{
-        if(error){
-            console.log(error);
+    fs.readFile('./data/employers.json', 'utf8', (err, employers) => {
+        if (err) {
+            console.log(err)
         }
-        else{
-            res.render('employers/index',{ title:'Employers List',
-            employers:JSON.parse(employers)
-        });
+        else {
+            console.log(employers);
+            res.render('employers/index', {
+                title: 'Employer List',
+                employers: JSON.parse(employers)
+            });
         }
-    })
-    
-
-    
+    }); 
 });
 
-//public visible
-module.exports=router;
+/* GET /create - display form to add an employer */
+router.get('/create', (req, res) => {
+    res.render('employers/create');
+});
+
+/* POST /create - submit form data to mongodb */
+router.post('/create', (req, res) => {
+    Employer.create(req.body, (err, newDocument) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.redirect('/employers');
+        }
+    });
+});
+
+// make public
+module.exports = router;
