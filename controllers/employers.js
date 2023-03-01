5825e24a32c0e2345df595184a90cb0118d4f155
jashan <jashannoor2001@gmail.com>
2023-03-01 13:22:44 -0500

    Added Files

--- controllers/employers.js
+++ controllers/employers.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 // add fs to read the data file
-const fs = require('fs');
+//const fs = require('fs');
 
 // use Employer model for CRUD w/mongoose
 const Employer = require('../models/employer');
+const City = require('../models/city');
 
 /* GET employers index (the module home page) */
 router.get('/', (req, res) => {
@@ -30,7 +31,7 @@ router.get('/', (req, res) => {
     });*/
 
     // get data from json file
-    fs.readFile('./data/employers.json', 'utf8', (err, employers) => {
+    /*fs.readFile('./data/employers.json', 'utf8', (err, employers) => {
         if (err) {
             console.log(err)
         }
@@ -41,12 +42,36 @@ router.get('/', (req, res) => {
                 employers: JSON.parse(employers)
             });
         }
-    }); 
+    }); */
+
+    // get data from mongodb using the Employer model
+    Employer.find((err, employers) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            console.log(employers);
+            res.render('employers/index', {
+                title: 'Employer List',
+                employers: employers
+            });
+        }
+    });
 });
 
 /* GET /create - display form to add an employer */
 router.get('/create', (req, res) => {
-    res.render('employers/create');
+    // use City model to fetch list of cities from db to populate city dropdown
+    City.find((err, cities) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.render('employers/create', {
+                cities: cities
+            });
+        }
+    }).sort('name');    
 });
 
 /* POST /create - submit form data to mongodb */
@@ -61,5 +86,53 @@ router.post('/create', (req, res) => {
     });
 });
 
+//GET delete
+
+router.get('/delete/:_id',(req, res)=>{
+    Employer.remove({_id:req.params._id}, (err)=>{
+
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.redirect('/employers')
+        }
+
+    })
+})
+
+
+router.get('/edit/:_id', (req, res) => {
+    Employer.findById(req.params._id, (err, employer) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            City.find((err, cities) => {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                     res.render('employers/edit', {
+                        employer: employer,
+                        title: 'Edit Employer Details',
+                        cities: cities
+                    });
+                }
+            }).sort('name');               
+        }
+    });
+});
+
+router.post('/edit/:_id', (req, res) => {
+    Employer.findByIdAndUpdate({ _id: req.params._id }, req.body, null, (err) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.redirect('/employers');
+        }
+    });
+});
 // make public
 module.exports = router;
